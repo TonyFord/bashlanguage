ffa9f2a30d4db4dee947538d4792857e28f818cf
TonyFord <mail2@strategy-investor.de>
2015-05-27 22:04:14 +0200

    bugfix

--- language.bash
+++ language.bash
@@ -4,12 +4,12 @@ function linit()
 {
 	system_ln=$(locale | grep LANGUAGE | cut -d= -f2 | cut -d_ -f1)
 
-	if [ -e "$1/lang/lang_$system_ln.json" ]; then
-		langjson=$(<$1/lang/lang_$system_ln.json)
+	if [ -e "$1/lang_$system_ln.json" ]; then
+		langjson=$(<$1/lang_$system_ln.json)
 	else 
 		# default language = en ################################
-		if [ -e "$1/lang/lang_en.json" ]; then
-			langjson=$(<$1/lang/lang_en.json)
+		if [ -e "$1/lang_en.json" ]; then
+			langjson=$(<$1/lang_en.json)
 		fi
 	fi
 }
